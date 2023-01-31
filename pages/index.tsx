aa2dd6d6b59e92f1968c52581abece416a64d5aa
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-01-31 15:45:31 -0500

    add board to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import BackDrop from "../components/backDrop";
 import NavigationBar from "../components/navigationBar";
 import MapInformationBar from "../components/mapInformationBar";
 import NodeCharacterBar from "../components/nodeCharacterBar";
+import Board from "../components/board/board";
 
 export default function Home() {
   return (
@@ -13,16 +13,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <BackDrop />
-        <div className=" flex flex-col h-screen">
-          <NavigationBar />
-          <div className="flex-grow mx-4 flex justify-between">
+      <main className="flex h-screen flex-col">
+        <NavigationBar />
+        <div className="relative h-full w-full">
+          <Board />
+          <div className="absolute left-4 top-0 bottom-0">
             <MapInformationBar />
-
-            <div>
-              <NodeCharacterBar />
-            </div>
+          </div>
+          <div className="absolute right-4 top-0 bottom-0">
+            <NodeCharacterBar />
           </div>
         </div>
       </main>
